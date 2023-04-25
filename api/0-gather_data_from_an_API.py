c58fe72eb94d87896389cf9d4bd15fa2084582d9
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2023-04-25 08:55:19 +0100

    data from api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-
-"""Gather data from an API"""
+"""Create an API to get todo list for employe"""
 
 import requests
-import sys
+from sys import argv
+
+url_base = "https://jsonplaceholder.typicode.com/users/"
 
 
 if __name__ == "__main__":
-    id = sys.argv[1]
-    user_response = requests.get(
-        "https://jsonplaceholder.typicode.com/users/" + id)
-    todos_response = requests.get("https://jsonplaceholder.typicode.com/todos")
+    employee = requests.get(url_base + argv[1]).json()
+    todo_list = requests.get(url_base + argv[1] + "/todos/").json()
+    completed_tasks = 0
+    text = ""
 
-    user_data = user_response.json()
-    todos_data = todos_response.json()
+    for task in todo_list:
+        if task['completed'] is True:
+            text += "\t {}\n".format(task['title'])
+            completed_tasks += 1
 
-    nbr_task = 0
-    nbr_cpt_task = 0
-    title_task = []
-
-    for i in todos_data:
-        if i["userId"] == int(id):
-            nbr_task += 1
-            if i["completed"] == 1:
-                title_task.append(i["title"])
-
-    """
-    dc_names = {k: v for d in user_data for k, v in d.items()}
-    dc_names = {}
-    for d in range(len(user_data)):
-        dc_names.update(user_data[d])
-    print(user_data[4])
-    """
-
-    print(
-        f"Employee {user_response.json().get('name')}"
-        f" is done with tasks({len(title_task)}/{nbr_task}):")
-
-    for i in title_task:
-        print(f"\t {i}")
+    print("Employee {} is done with tasks({}/20):\n{}"
+          .format(employee['name'],
+                  completed_tasks,
+                  text), end="")
